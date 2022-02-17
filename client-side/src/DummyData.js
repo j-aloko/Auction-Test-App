3e153636328af4729d1db2391d00952e8fcfef92
j-aloko <jaloko@st.ug.edu.gh>
2022-02-16 23:12:32 -1200

    creating my dummy data

---
+++ client-side/src/DummyData.js
@@ -0,0 +1,335 @@
+export const users = [
+  {
+    id: 1,
+    firstName: "Jon",
+    lastName: "Snow",
+    email: "jonsnow@gmail.com",
+    location: "Ghana",
+    totalbids: 3,
+    bidsWon: 1,
+  },
+  {
+    id: 2,
+    firstName: "Jon",
+    lastName: "Snow",
+    email: "jonsnow@gmail.com",
+    location: "Ghana",
+    totalbids: 3,
+    bidsWon: 1,
+  },
+];
+
+export const products = [
+  {
+    id: 1,
+    title: "Vintage Car",
+    img: "https://images.unsplash.com/photo-1564758316866-432196a42030?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1000&q=80",
+    minimumBid: 100000,
+    description:
+      "built between the time period of 1919 and 1930. Whereas, some consider the time period to be 1919 to 1925. However, unlike antique cars and classic cars, the vintage cars do not lose their value in case they are modified",
+    currentBid: 150000,
+    endDate: "February 22, 2022 00:00:00",
+    bidders: [
+      {
+        firstName: "Jon ",
+        lastName: "Snow",
+        amount: 160000,
+        budget: 200000,
+        autoBid: false,
+      },
+      {
+        firstName: "jane",
+        lastName: "snow",
+        amount: 160000,
+        budget: 200000,
+        autoBid: false,
+      },
+    ],
+  },
+  {
+    id: 2,
+    title: "Classic Sound Player",
+    img: "https://images.unsplash.com/photo-1518893883800-45cd0954574b?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1000&q=80",
+    minimumBid: 100000,
+    description:
+      "built between the time period of 1919 and 1930. Whereas, some consider the time period to be 1919 to 1925. However, unlike antique cars and classic cars, the vintage cars do not lose their value in case they are modified",
+    currentBid: 150000,
+    endDate: "February 22, 2022 00:00:00",
+    bidders: [
+      {
+        firstName: "Jon ",
+        lastName: "Snow",
+        amount: 160000,
+        budget: 200000,
+        autoBid: false,
+      },
+      {
+        firstName: "jane",
+        lastName: "snow",
+        amount: 160000,
+        budget: 200000,
+        autoBid: false,
+      },
+    ],
+  },
+  {
+    id: 3,
+    title: "Sailors Watch",
+    img: "https://images.unsplash.com/photo-1516456277948-c81597a8d1be?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1000&q=80",
+    minimumBid: 100000,
+    description:
+      "built between the time period of 1919 and 1930. Whereas, some consider the time period to be 1919 to 1925. However, unlike antique cars and classic cars, the vintage cars do not lose their value in case they are modified",
+    currentBid: 150000,
+    endDate: "February 22, 2022 00:00:00",
+    bidders: [
+      {
+        firstName: "Jon ",
+        lastName: "Snow",
+        amount: 160000,
+        budget: 200000,
+        autoBid: false,
+      },
+      {
+        firstName: "jane",
+        lastName: "snow",
+        amount: 160000,
+        budget: 200000,
+        autoBid: false,
+      },
+    ],
+  },
+  {
+    id: 4,
+    title: "Italian Sculpture",
+    img: "https://images.unsplash.com/photo-1615497994569-db84b0f1dd55?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1000&q=80",
+    minimumBid: 100000,
+    description:
+      "built between the time period of 1919 and 1930. Whereas, some consider the time period to be 1919 to 1925. However, unlike antique cars and classic cars, the vintage cars do not lose their value in case they are modified",
+    currentBid: 150000,
+    endDate: "February 22, 2022 00:00:00",
+    bidders: [
+      {
+        firstName: "Jon ",
+        lastName: "Snow",
+        amount: 160000,
+        budget: 200000,
+        autoBid: false,
+      },
+      {
+        firstName: "jane",
+        lastName: "snow",
+        amount: 160000,
+        budget: 200000,
+        autoBid: false,
+      },
+    ],
+  },
+  {
+    id: 5,
+    title: "Vintage Camera",
+    img: "https://images.unsplash.com/photo-1604125884462-125b077cd0d6?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1000&q=80",
+    minimumBid: 100000,
+    description:
+      "built between the time period of 1919 and 1930. Whereas, some consider the time period to be 1919 to 1925. However, unlike antique cars and classic cars, the vintage cars do not lose their value in case they are modified",
+    currentBid: 150000,
+    endDate: "February 22, 2022 00:00:00",
+    bidders: [
+      {
+        firstName: "Jon ",
+        lastName: "Snow",
+        amount: 160000,
+        budget: 200000,
+        autoBid: false,
+      },
+      {
+        firstName: "jane",
+        lastName: "snow",
+        amount: 160000,
+        budget: 200000,
+        autoBid: false,
+      },
+    ],
+  },
+  {
+    id: 6,
+    title: "Antique Clock",
+    img: "https://images.unsplash.com/photo-1627307284579-327ea0c7de14?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1000&q=80",
+    minimumBid: 100000,
+    description:
+      "built between the time period of 1919 and 1930. Whereas, some consider the time period to be 1919 to 1925. However, unlike antique cars and classic cars, the vintage cars do not lose their value in case they are modified",
+    currentBid: 150000,
+    endDate: "February 22, 2022 00:00:00",
+    bidders: [
+      {
+        firstName: "Jon ",
+        lastName: "Snow",
+        amount: 160000,
+        budget: 200000,
+        autoBid: false,
+      },
+      {
+        firstName: "jane",
+        lastName: "snow",
+        amount: 160000,
+        budget: 200000,
+        autoBid: false,
+      },
+    ],
+  },
+  {
+    id: 7,
+    title: "Vintage Sewing Machine",
+    img: "https://images.unsplash.com/photo-1466027397211-20d0f2449a3f?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1000&q=80",
+    minimumBid: 100000,
+    description:
+      "built between the time period of 1919 and 1930. Whereas, some consider the time period to be 1919 to 1925. However, unlike antique cars and classic cars, the vintage cars do not lose their value in case they are modified",
+    currentBid: 150000,
+    endDate: "February 22, 2022 00:00:00",
+    bidders: [
+      {
+        firstName: "Jon ",
+        lastName: "Snow",
+        amount: 160000,
+        budget: 200000,
+        autoBid: false,
+      },
+      {
+        firstName: "jane",
+        lastName: "snow",
+        amount: 160000,
+        budget: 200000,
+        autoBid: false,
+      },
+    ],
+  },
+  {
+    id: 8,
+    title: "Old School Typewriter",
+    img: "https://images.unsplash.com/photo-1611517049947-acf3777c55bf?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1000&q=80",
+    minimumBid: 100000,
+    description:
+      "built between the time period of 1919 and 1930. Whereas, some consider the time period to be 1919 to 1925. However, unlike antique cars and classic cars, the vintage cars do not lose their value in case they are modified",
+    currentBid: 150000,
+    endDate: "February 22, 2022 00:00:00",
+    bidders: [
+      {
+        firstName: "Jon ",
+        lastName: "Snow",
+        amount: 160000,
+        budget: 200000,
+        autoBid: false,
+      },
+      {
+        firstName: "jane",
+        lastName: "snow",
+        amount: 160000,
+        budget: 200000,
+        autoBid: false,
+      },
+    ],
+  },
+  {
+    id: 9,
+    title: "Vintage Furniture",
+    img: "https://images.unsplash.com/photo-1600849128602-c47832664de4?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1000&q=80",
+    minimumBid: 100000,
+    description:
+      "built between the time period of 1919 and 1930. Whereas, some consider the time period to be 1919 to 1925. However, unlike antique cars and classic cars, the vintage cars do not lose their value in case they are modified",
+    currentBid: 150000,
+    endDate: "February 22, 2022 00:00:00",
+    bidders: [
+      {
+        firstName: "Jon ",
+        lastName: "Snow",
+        amount: 160000,
+        budget: 200000,
+        autoBid: false,
+      },
+      {
+        firstName: "jane",
+        lastName: "snow",
+        amount: 160000,
+        budget: 200000,
+        autoBid: false,
+      },
+    ],
+  },
+  {
+    id: 10,
+    title: "1700 Roman Armor",
+    img: "https://images.unsplash.com/photo-1553986782-9f6de60b51b4?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1000&q=80",
+    minimumBid: 100000,
+    description:
+      "built between the time period of 1919 and 1930. Whereas, some consider the time period to be 1919 to 1925. However, unlike antique cars and classic cars, the vintage cars do not lose their value in case they are modified",
+    currentBid: 150000,
+    endDate: "February 22, 2022 00:00:00",
+    bidders: [
+      {
+        firstName: "Jon ",
+        lastName: "Snow",
+        amount: 160000,
+        budget: 200000,
+        autoBid: false,
+      },
+      {
+        firstName: "jane",
+        lastName: "snow",
+        amount: 160000,
+        budget: 200000,
+        autoBid: false,
+      },
+    ],
+  },
+  {
+    id: 11,
+    title: "1933 Ford Hot Rod",
+    img: "https://images.unsplash.com/photo-1526284059721-5519814a2274?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1000&q=80",
+    minimumBid: 100000,
+    description:
+      "built between the time period of 1919 and 1930. Whereas, some consider the time period to be 1919 to 1925. However, unlike antique cars and classic cars, the vintage cars do not lose their value in case they are modified",
+    currentBid: 150000,
+    endDate: "February 22, 2022 00:00:00",
+    bidders: [
+      {
+        firstName: "Jon ",
+        lastName: "Snow",
+        amount: 160000,
+        budget: 200000,
+        autoBid: false,
+      },
+      {
+        firstName: "jane",
+        lastName: "snow",
+        amount: 160000,
+        budget: 200000,
+        autoBid: false,
+      },
+    ],
+  },
+  {
+    id: 12,
+    title: "Viking Warrior",
+    img: "https://images.unsplash.com/photo-1589666685762-afbbee29cd08?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1000&q=80",
+    minimumBid: 100000,
+    description:
+      "built between the time period of 1919 and 1930. Whereas, some consider the time period to be 1919 to 1925. However, unlike antique cars and classic cars, the vintage cars do not lose their value in case they are modified",
+    currentBid: 150000,
+    endDate: "February 22, 2022 00:00:00",
+    bidders: [
+      {
+        firstName: "Jon ",
+        lastName: "Snow",
+        amount: 160000,
+        budget: 200000,
+        autoBid: false,
+      },
+      {
+        firstName: "jane",
+        lastName: "snow",
+        amount: 160000,
+        budget: 200000,
+        autoBid: false,
+      },
+    ],
+  },
+];
