99c2c939536b8badb1a99b5d1afefa88e619f1ed
j-aloko <jaloko@st.ug.edu.gh>
2022-02-17 11:46:37 -1200

    initializing express server and mongoose connection

---
+++ server-side/index.js
@@ -0,0 +1,28 @@
+const express = require("express");
+const mongoose = require("mongoose");
+const dotenv = require("dotenv");
+const app = express();
+const cors = require("cors");
+const morgan = require("morgan");
+const helmet = require("helmet");
+
+dotenv.config();
+
+app.listen(process.env.PORT || 8080, () => {
+  console.log("server is running");
+});
+
+mongoose
+  .connect(process.env.MONGO_URL, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  })
+  .then(() => console.log("connected to Mongo_DB"))
+  .catch((err) => console.log("MongoDB connection failed"));
+
+app.use(cors({ origin: "http://localhost:3000", credentials: true }));
+
+app.use(morgan("common"));
+app.use(helmet());
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
