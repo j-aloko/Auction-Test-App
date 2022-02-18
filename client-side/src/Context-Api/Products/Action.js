430bfc0157de5ff283c8f3d9709d09ac9f843f82
j-aloko <jaloko@st.ug.edu.gh>
2022-02-17 12:59:41 -1200

    initializing context api for management of product state across app

---
+++ client-side/src/Context-Api/Products/Action.js
@@ -0,0 +1,43 @@
+//GET PRODUCTS
+export const getProductsStart = () => ({
+  type: "GET_PRODUCTS_START",
+});
+
+export const getProductsSuccess = (products) => ({
+  type: "GET_PRODUCTS_SUCCESS",
+  payload: products,
+});
+
+export const getProductsFailure = () => ({
+  type: "GET_PRODUCTS_FAILURE",
+});
+
+//CREATE PRODUCT
+
+export const createProductStart = () => ({
+  type: "CREATE_PRODUCT_START",
+});
+
+export const createProductSuccess = (product) => ({
+  type: "CREATE_PRODUCT_SUCCESS",
+  payload: product,
+});
+
+export const createProductFailure = () => ({
+  type: "CREATE_PRODUCT_FAILURE",
+});
+
+//UPDATE PRODUCTS
+
+export const updateProductStart = () => ({
+  type: "UPDATE_PRODUCT_START",
+});
+
+export const updateProductSuccess = (product) => ({
+  type: "UPDATE_PRODUCT_SUCCESS",
+  payload: product,
+});
+
+export const updateProductFailure = () => ({
+  type: "UPDATE_PRODUCT_FAILURE",
+});
--- client-side/src/index.js
+++ client-side/src/index.js
@@ -1,14 +1,17 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+import ReactDOM from "react-dom";
+import "./index.css";
+import App from "./App";
+import reportWebVitals from "./reportWebVitals";
+import { ProductsContextProvider } from "./Context-Api/Products/Context";
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <ProductsContextProvider>
+      <App />
+    </ProductsContextProvider>
   </React.StrictMode>,
-  document.getElementById('root')
+  document.getElementById("root")
 );
 
 // If you want to start measuring performance in your app, pass a function
